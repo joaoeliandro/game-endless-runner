5ed29582398c875455f484cfe031a602663463ea
João Eliandro <eliandrogermano@gmail.com>
2020-06-24 03:51:19 -0300

    Added local server

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname, 'style')));
+app.use('/images', express.static(path.resolve(__dirname, 'images')));
+app.use(express.static(path.resolve(__dirname, 'sounds')));
+
+app.listen(3333);
