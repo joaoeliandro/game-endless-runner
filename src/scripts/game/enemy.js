f119dfdafe26d498c33d6a726c56d8aa692e2fc9
João Eliandro <eliandrogermano@gmail.com>
2020-06-24 23:00:55 -0300

    Added class enemy

---
+++ src/scripts/game/enemy.js
@@ -0,0 +1,7 @@
+class Enemy extends Animated {
+    constructor(typeMatrix, image, x, widthCharacter, heightCharacter,
+        widthSprite, heightSprite) {
+        super(typeMatrix, image, x, widthCharacter, heightCharacter,
+            widthSprite, heightSprite);
+    }
+}
