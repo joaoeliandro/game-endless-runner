b175c9d12008b297d4437d643cfc5b0f0a26cf44
João Eliandro <eliandrogermano@gmail.com>
2020-06-24 23:34:07 -0300

    Added move function

--- src/scripts/game/enemy.js
+++ src/scripts/game/enemy.js
@@ -1,7 +1,15 @@
 class Enemy extends Animated {
-    constructor(typeMatrix, image, x, widthCharacter, heightCharacter,
+    constructor(typeMatrix, image, x, widthEnemy, heightEnemy,
         widthSprite, heightSprite) {
-        super(typeMatrix, image, x, widthCharacter, heightCharacter,
+        super(typeMatrix, image, x, widthEnemy, heightEnemy,
             widthSprite, heightSprite);
+        
+        this.speed = 9;
+    }
+
+    move() {
+        this.x = this.x - this.speed;
+        
+        if (this.x < -widthEnemy) this.x = width;
     }
 }
