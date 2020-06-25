1e72a83af85893120ee8a75097a86941ba5e0c31
João Eliandro <eliandrogermano@gmail.com>
2020-06-24 21:49:04 -0300

    Routes to files statics

--- src/server.js
+++ src/server.js
@@ -9,6 +9,6 @@ app.use(cors());
 app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(express.static(path.resolve(__dirname, 'style')));
 app.use('/images', express.static(path.resolve(__dirname, 'images')));
-app.use(express.static(path.resolve(__dirname, 'sounds')));
+app.use('/sounds', express.static(path.resolve(__dirname, 'sounds')));
 
 app.listen(3333);
