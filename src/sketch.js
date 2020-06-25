82359f6ccc21acb4f5690375e22e547cf047b847
João Eliandro <eliandrogermano@gmail.com>
2020-06-24 21:49:39 -0300

    Refactored code

--- src/sketch.js
+++ src/sketch.js
@@ -1,7 +1,41 @@
+let imageScenario;
+let character;
+let scenario;
+let imageCharacter;
+const heightCharacter = 155;
+const widthCharacter = 130;
+let soundGame;
+let tapped = false;
+
+
+function preload() {
+  imageScenario = loadImage('http://localhost:3333/images/scenario/forest.png');
+  imageCharacter = loadImage('http://localhost:3333/images/character/running.png');
+  soundGame = loadSound('http://localhost:3333/sounds/sound_game.mp3');
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  let cnv = createCanvas(windowWidth, windowHeight);
+
+  scenario = new Scenario(imageScenario, 4);
+  character = new Character(imageCharacter);
+
+  frameRate(40);
+  cnv.mousePressed(toggleSound);
 }
 
 function draw() {
-  background(220);
+  scenario.show();
+  scenario.move();
+
+  character.show();
+
+  if (!tapped) {
+    text('tap to play', width / 2, 400);
+  }
+}
+
+function toggleSound() {
+  tapped = true;
+  soundGame.loop();
 }
