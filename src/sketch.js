ddc42ae7143f192c44f6676b083c88ab4376fb3a
João Eliandro <eliandrogermano@gmail.com>
2020-06-24 23:34:17 -0300

    Added move function enemy

--- src/sketch.js
+++ src/sketch.js
@@ -1,16 +1,24 @@
 let imageScenario;
-let character;
-let scenario;
 let imageCharacter;
-const heightCharacter = 155;
-const widthCharacter = 130;
+let imageEnemy;
+
+let scenario;
+let character;
+let enemy;
 let soundGame;
+
+const widthCharacter = 130;
+const heightCharacter = 155;
+const widthEnemy = 70;
+const heightEnemy = 70;
+
 let tapped = false;
 
 
 function preload() {
   imageScenario = loadImage('http://localhost:3333/images/scenario/forest.png');
   imageCharacter = loadImage('http://localhost:3333/images/character/running.png');
+  imageEnemy = loadImage('http://localhost:3333/images/enemies/droplet.png');
   soundGame = loadSound('http://localhost:3333/sounds/sound_game.mp3');
 }
 
@@ -18,7 +26,10 @@ function setup() {
   let cnv = createCanvas(windowWidth, windowHeight);
 
   scenario = new Scenario(imageScenario, 4);
-  character = new Character(imageCharacter);
+  character = new Character('character', imageCharacter,
+    80, widthCharacter, heightCharacter, 220, 270);
+  enemy = new Enemy('enemy', imageEnemy,
+    width, widthEnemy, heightEnemy, 104, 104);
 
   frameRate(40);
   cnv.mousePressed(toggleSound);
@@ -29,6 +40,9 @@ function draw() {
   scenario.move();
 
   character.show();
+
+  enemy.show();
+  enemy.move();
 
   if (!tapped) {
     text('tap to play', width / 2, 400);
