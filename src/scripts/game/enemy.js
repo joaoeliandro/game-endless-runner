92baafd391d426048e1cdfdd6e1734ed7f2ec438
João Eliandro <eliandrogermano@gmail.com>
2020-06-26 06:58:51 -0300

    Added delay

--- src/scripts/game/enemy.js
+++ src/scripts/game/enemy.js
@@ -1,15 +1,17 @@
 class Enemy extends Animated {
-    constructor(typeMatrix, image, x, widthEnemy, heightEnemy,
-        widthSprite, heightSprite) {
-        super(typeMatrix, image, x, widthEnemy, heightEnemy,
+    constructor(typeMatrix, image, x, variableY, widthEnemy, heightEnemy,
+        widthSprite, heightSprite, speed, delay) {
+        super(typeMatrix, image, x, variableY, widthEnemy, heightEnemy,
             widthSprite, heightSprite);
-        
-        this.speed = 9;
+
+        this.speed = speed;
+        this.delay = delay;
+        this.x = width + this.delay;
     }
 
     move() {
         this.x = this.x - this.speed;
-        
-        if (this.x < -widthEnemy) this.x = width;
+
+        if (this.x < -this.widthCharacter - this.delay) this.x = width;
     }
 }
