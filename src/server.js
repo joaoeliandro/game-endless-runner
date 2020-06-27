4fcfed312c5ffcb6d64caf3cf186330d5edab4d4
João Eliandro <eliandrogermano@gmail.com>
2020-06-27 05:52:05 -0300

    Created route and use static files

--- src/server.js
+++ src/server.js
@@ -6,9 +6,15 @@ const app = express();
 
 app.use(cors());
 
-app.use(express.static(path.resolve(__dirname, 'public')));
-app.use(express.static(path.resolve(__dirname, 'style')));
+app.use('/public', express.static(path.resolve(__dirname, 'public')));
+app.use('/style', express.static(path.resolve(__dirname, 'style')));
+app.use('/scripts', express.static(path.resolve(__dirname, 'scripts')));
+app.use('/sketch', express.static(path.resolve(__dirname, 'sketch.js')));
 app.use('/images', express.static(path.resolve(__dirname, 'images')));
 app.use('/sounds', express.static(path.resolve(__dirname, 'sounds')));
+
+app.get('/', (request, response) =>
+    response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+);
 
 app.listen(3333);
