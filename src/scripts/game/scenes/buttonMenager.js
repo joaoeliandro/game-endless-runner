8226fd6488d22d08d3a7778053a2d174e1263273
João Eliandro <eliandrogermano@gmail.com>
2020-06-29 06:29:36 -0300

    Created new classes

---
+++ src/scripts/game/scenes/buttonMenager.js
@@ -0,0 +1,27 @@
+class ButtonMenager {
+    constructor(textButton, posicionX, posicionY) {
+        this.textButton = textButton;
+        this.posicionX = posicionX;
+        this.posicionY = posicionY;
+
+        this.button = createButton(this.textButton);
+        this.button.mousePressed(() => this._changeScene());
+        this.button.addClass('button-starter-screen');
+    }
+
+    draw() {
+        this.button.position(this.posicionX, this.posicionY);
+        this.button.center('horizontal');
+    }
+
+    _changeScene() {
+        this.button.remove();
+        currentScene = 'game';
+        this._toggleSoundGame();
+    }
+
+    _toggleSoundGame() {
+        tapped = true;
+        // soundGame.loop();
+    }
+}
