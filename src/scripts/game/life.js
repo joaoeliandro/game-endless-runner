b8b9f2cec053d4a197581f7708300624555408ae
João Eliandro <eliandrogermano@gmail.com>
2020-06-30 05:27:00 -0300

    Created class

---
+++ src/scripts/game/life.js
@@ -0,0 +1,36 @@
+class Life {
+    constructor(maxLife, starterLife) {
+        this.maxLife = maxLife;
+        this.starterLife = starterLife;
+
+        this.lifes = this.starterLife;
+
+        this.widthHeart = 45;
+        this.heightHeart = 45;
+
+        this.margin;
+        this.position;
+        this.starterX = 30;
+        this.positionY = 20;
+    }
+
+    draw() {
+        for (let hearts = 0; hearts < this.lifes; hearts++) {
+            this.margin = hearts * 30;
+            this.position = this.starterX * (hearts + 1);
+
+            image(imageHeart, this.position + this.margin, this.positionY,
+                this.widthHeart, this.heightHeart);
+        }
+    }
+
+    getLife() {
+        if (this.lifes <= this.maxLife) this.lifes++;
+    }
+
+    loseLife() {
+        if (this.lifes < 1) return true;
+        this.lifes--;
+        return false;
+    }
+}
